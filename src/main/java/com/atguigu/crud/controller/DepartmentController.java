f001495382b3dcdf7cb088ba3f7df9f09a45562a
wangjun <1150993274@qq.com>
2020-07-26 16:10:02 +0800

    员工列表

---
+++ src/main/java/com/atguigu/crud/controller/DepartmentController.java
@@ -0,0 +1,13 @@
+package com.atguigu.crud.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 处理和部门有关的请求
+ * @author lfy
+ *
+ */
+@Controller
+public class DepartmentController {
+
+}
